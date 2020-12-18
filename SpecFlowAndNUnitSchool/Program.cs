4f98dedae03f7b9ed1415b3daa65fcf3cdf735c9
vorobiova.a <vorobbyova@gmail.com>
2020-12-18 20:07:32 +0200

    rewrote the project

---
+++ SpecFlowAndNUnitSchool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SpecFlowAndNUnitSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.demoqa.com";
+        }
+    }
+}
