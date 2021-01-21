bd8b64144cbbc809caa9fcd39b2c91d3ff59c4be
vorobiova.a <vorobbyova@gmail.com>
2021-01-21 17:14:43 +0200

    created a new project where there are different approaches of working with models and tables Entity Framework

---
+++ LearningEntityFrameWork/ContextApp.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LearningEntityFrameWork
+{
+    class ContextApp :DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public ContextApp()
+        {
+            Database.EnsureDeleted();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TestDB_001;Trusted_Connection=True;");
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Tablet>();
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Ignore<Company>(); 
+        //}
+    }
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        //navigation preporty
+        public Company Manufacturer { get; set; }
+
+    }
+
+     [NotMapped]
+     public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+    public class Tablet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
---
+++ LearningEntityFrameWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningEntityFrameWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
