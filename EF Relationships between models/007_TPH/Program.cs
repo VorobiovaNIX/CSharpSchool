ea8ca3f0bbe3e8730c3815ecd27e2f8938343d61
vorobiova.a <vorobbyova@gmail.com>
2021-01-26 22:43:30 +0200

    add values to table using classes with inheritance

--- EF Relationships between models/007_TPH/Program.cs
+++ EF Relationships between models/007_TPH/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _007_TPH
 {
@@ -6,7 +7,41 @@ namespace _007_TPH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (Context db = new Context())
+            {
+                User user1 = new User { Name = "Tom" };
+                User user2 = new User { Name = "Bob" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+
+                Employee employee = new Employee { Name = "Shon", Salary = 500 };
+                db.Employees.Add(employee);
+
+                Manager manager = new Manager { Name = "Robert", Departament = "IT" };
+                db.Managers.Add(manager);
+
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+                Console.WriteLine("\n All users");
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+
+                Console.WriteLine("\n All employees");
+                foreach (var emp in db.Employees.ToList())
+                {
+                    Console.WriteLine(emp.Name);
+                }
+
+                Console.WriteLine("\n All managers");
+                foreach (var manag in db.Managers.ToList())
+                {
+                    Console.WriteLine(manag.Name);
+                }
+            }
         }
     }
 }
