d7e5f53140baf9628e2f550728b39c3b28b6c407
vorobiova.a <vorobbyova@gmail.com>
2021-01-28 23:58:34 +0200

    practice Entity Framework requests LINQ

---
+++ EF queries LINQ to Entities/001_Queries_LINQ to Entities/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace _001_Queries_LINQ_to_Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Context db = new Context())
+            {
+                if (db.Phones.Count()==0)
+                {
+
+                }
+
+                var phones = (from phone in db.Phones.Include(p => p.Company)
+                              where phone.CompanyId == 1
+                              select phone).ToList();
+
+                foreach (var phone in phones)
+                {
+                    Console.WriteLine($"{phone.Name} ({phone.Price}) - {phone.Company?.Name}");
+                }
+            }
+            Console.Read();
+
+            Console.WriteLine(new string ('-',20));
+
+            using (Context db = new Context())
+            {
+                var phones = db.Phones.Include(p => p.Company).Where(p => p.CompanyId == 1);
+                foreach (var phone in phones)
+                {
+                    Console.WriteLine($"{phone.Name} ({phone.Price}) - {phone.Company?.Name}");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
